32a6d914e627ac4f0e8f68effc8c3309836c5479
donotcoffee <donotcoffee@gmail.com>
2017-09-17 00:13:44 +0800

    fix:ip工具类

--- core-utils/src/main/java/com/thinkme/utils/net/IPUtil.java
+++ core-utils/src/main/java/com/thinkme/utils/net/IPUtil.java
@@ -22,7 +22,6 @@ import java.util.List;
  * @author calvin
  */
 public class IPUtil {
-
 	/**
 	 * 从InetAddress转化到int, 传输和存储时, 用int代表InetAddress是最小的开销.
 	 * 
@@ -149,32 +148,48 @@ public class IPUtil {
 	}
 
 	/**
-	 * 获取连接WEB服务器的真实IP，前端有UTR的拦截请求
+	 * <p>
+	 *  获取客户端的IP地址的方法是：request.getRemoteAddr()，这种方法在大部分情况下都是有效的。
+	 *  但是在通过了Apache,Squid等反向代理软件就不能获取到客户端的真实IP地址了，如果通过了多级反向代理的话，
+	 *  X-Forwarded-For的值并不止一个，而是一串IP值， 究竟哪个才是真正的用户端的真实IP呢？
+	 *  答案是取X-Forwarded-For中第一个非unknown的有效IP字符串。
+	 *  例如：X-Forwarded-For：192.168.1.110, 192.168.1.120,
+	 *  192.168.1.130, 192.168.1.100 用户真实IP为： 192.168.1.110
+	 *  </p>
+	 *
+	 * @param request
+	 * @return
 	 */
 	public static String getIpAddr(HttpServletRequest request) {
-
-		String ip = request.getHeader("X-Forwarded-For");
-
+		String ip = request.getHeader("x-forwarded-for");
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
-
-		} else {
-			String[] ipArray = ip.split("\\,");
-			ip = ipArray[0];//  获取第一个IP
 		}
-
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
 		}
-
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("clientip");
-		}
-
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
+			if (ip.equals("127.0.0.1")) {
+				/** 根据网卡取本机配置的IP */
+				InetAddress inet = null;
+				try {
+					inet = InetAddress.getLocalHost();
+					ip = inet.getHostAddress();
+				} catch (UnknownHostException e) {
+//					logger.severe("IpHelper error." + e.toString());
+				}
+			}
 		}
-
+		/**
+		 * 对于通过多个代理的情况， 第一个IP为客户端真实IP,多个IP按照','分割 "***.***.***.***".length() =
+		 * 15
+		 */
+		if (ip != null && ip.length() > 15) {
+			if (ip.indexOf(",") > 0) {
+				ip = ip.substring(0, ip.indexOf(","));
+			}
+		}
 		return ip;
 	}
 }
